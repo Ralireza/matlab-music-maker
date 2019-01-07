772d27222691b7e3281a011da6be27e29071c148
4lrz <alirezarezaie0101@gmail.com>
2019-01-07 16:59:21 +0330

    add axes to ui

--- audio_player.m
+++ audio_player.m
@@ -114,3 +114,21 @@ if strcmp(get(handles.player, 'Running'), 'off')
         pause(0.5);
     end
 end
+
+
+% --- Executes on selection change in popupmenu1.
+function popupmenu1_Callback(hObject, eventdata, handles)
+% hObject    handle to popupmenu1 (see GCBO)
+% eventdata  reserved - to be defined in a future version of MATLAB
+% handles    structure with handles and user data (see GUIDATA)
+
+% Hints: contents = cellstr(get(hObject,'String')) returns popupmenu1 contents as cell array
+%        contents{get(hObject,'Value')} returns selected item from popupmenu1
+
+
+% --- Executes during object creation, after setting all properties.
+function popupmenu1_CreateFcn(hObject, eventdata, handles)
+
+if ispc && isequal(get(hObject,'BackgroundColor'), get(0,'defaultUicontrolBackgroundColor'))
+    set(hObject,'BackgroundColor','white');
+end
